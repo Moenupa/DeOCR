509a9bb9d94ab21976ea1228e09abd5c8636d7c9
Moenupa <meng.wang@cair-cas.org.hk>
2025-10-31 21:39:56 +0800

    feat: support saveimage opt

--- src/deocr/engine/playwright/pdf2image.py
+++ src/deocr/engine/playwright/pdf2image.py
@@ -1,4 +1,7 @@
-import asyncio
+import os
+from typing import TYPE_CHECKING
+
+from tqdm.contrib.concurrent import process_map
 
 try:
     import pymupdf
@@ -6,25 +9,52 @@ except ImportError:
     pymupdf = None
 
 
+if TYPE_CHECKING:
+    from PIL.Image import Image
+
+
+if "DEOCR_PDF2IMAGE_WORKERS" in os.environ:
+    MAX_WORKERS = int(os.environ["DEOCR_PDF2IMAGE_WORKERS"])
+else:
+    MAX_WORKERS = None
+
+
 def get_image_path(subfolder: str, i: int, total: int, extension: str) -> str:
     return f"{subfolder}/{i:010d}-{total:010d}.{extension}"
 
 
-def pdf2image(pdf_bytes: bytes, subfolder: str, dpi: int, extension: str) -> list[str]:
+def _worker(args):
+    pdf_bytes, subfolder, dpi, extension, save_images, i, n_pages = args
     with pymupdf.Document(stream=pdf_bytes, filetype="pdf") as pdf_doc:
-        n_pages = len(pdf_doc)
-        image_paths: list[str] = []
-        for i in range(n_pages):
-            page_pdf = pdf_doc.load_page(i)
-            pix = page_pdf.get_pixmap(dpi=dpi)
+        page_pdf = pdf_doc.load_page(i)
+        pix = page_pdf.get_pixmap(dpi=dpi)
+        if save_images:
             save_to = get_image_path(subfolder, i, n_pages, extension)
             pix.save(save_to)
-            image_paths.append(save_to)
+            return save_to
+        else:
+            return pix.pil_image()
 
-    return image_paths
 
+def pdf2image(
+    pdf_bytes: bytes, subfolder: str, dpi: int, extension: str, save_images: bool = True
+) -> list[str] | list["Image"]:
+    with pymupdf.Document(stream=pdf_bytes, filetype="pdf") as pdf_doc:
+        n_pages = len(pdf_doc)
+        if n_pages == 1:
+            it = _worker(
+                (pdf_bytes, subfolder, dpi, extension, save_images, 0, n_pages)
+            )
+            return [it]
 
-async def pdf2image_async(
-    pdf_bytes: bytes, subfolder: str, dpi: int, extension: str
-) -> list[str]:
-    return await asyncio.to_thread(pdf2image, pdf_bytes, subfolder, dpi, extension)
+    # distribute saving work using tqdm
+    out = process_map(
+        _worker,
+        [
+            (pdf_bytes, subfolder, dpi, extension, save_images, i, n_pages)
+            for i in range(n_pages)
+        ],
+        max_workers=None if MAX_WORKERS is None else min(n_pages, MAX_WORKERS),
+        chunksize=1,
+    )
+    return out
